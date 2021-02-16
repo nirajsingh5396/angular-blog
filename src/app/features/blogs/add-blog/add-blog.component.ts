ec951fde726bb01ff2770fed08a4d4363e1ee350
niraj <niraj.kumar@plexcel.com>
2021-02-17 01:30:38 +0530

    Implemented all crud operation

--- src/app/features/blogs/add-blog/add-blog.component.ts
+++ src/app/features/blogs/add-blog/add-blog.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 import { BlogsService } from '../blogs.service';
 import { Blogs } from '../models/blogs.model';
@@ -11,7 +14,8 @@ import { Blogs } from '../models/blogs.model';
   styleUrls: ['./add-blog.component.scss']
 })
 export class AddBlogComponent implements OnInit {
-
+  @Input() blog: Blogs;
+  @Input() title: string;
   addBlogForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
@@ -25,9 +29,9 @@ export class AddBlogComponent implements OnInit {
 
   buildBlogForm() {
     this.addBlogForm = this.formBuilder.group({
-      title: [null, [Validators.required]],
-      subTitle: [null, [Validators.required]],
-      desc: [null, [Validators.required]],
+      title: [!!this.blog ? this.blog.title : null, [Validators.required], this.checkTitleExstence.bind(this)],
+      subTitle: [!!this.blog ? this.blog.subTitle : null, [Validators.required]],
+      desc: [!!this.blog ? this.blog.desc : null, [Validators.required]],
     });
   }
 
@@ -36,14 +40,52 @@ export class AddBlogComponent implements OnInit {
       return;
     }
     const blog = this.addBlogForm.value as Blogs;
+
+    if (!!this.title) {
+      return this.updateBlog(blog)
+    }
     this.blogService.createBlog(blog)
       .subscribe((res) => {
-        console.log(res);
         if (res.createdBlog) {
           this.notify.showNotification('Blog created successfully!', 'top', 'success');
           this.router.navigate(['/blogs/list']);
         }
       }, (err) => this.notify.showNotification('Something went wrong', 'bottom', 'error'));
+  }
+
+  updateBlog(blog: Blogs) {
+    this.blogService.updateBlog(blog)
+      .subscribe((res) => {
+        if (res.updatedBlog) {
+          this.notify.showNotification('Blog updated successfully!', 'top', 'success');
+          this.router.navigate(['/blogs/list']);
+        }
+      }, (err) => this.notify.showNotification('Something went wrong', 'bottom', 'error'));
+  }
+
+  getTitleErrormsg(){
+    if (this.addBlogForm.controls.title.hasError('required')) {
+      return 'Title is required';
+    } if (this.addBlogForm.controls.title.hasError('existingTitle')) {
+      return 'Title already exists';
+    }
+  }
+
+  checkTitleExstence(emailControl: FormControl): Observable<any> {
+    if(!!this.title){
+      return of(null);
+    }
+    const title = emailControl.value;
+    return this.blogService.getBlogByTitle(title)
+      .pipe(
+        map(blog => {
+          if (blog) {
+            return { existingTitle: true }
+          } else {
+            return null;
+          }
+        })
+      );
   }
 
 }
--- src/app/features/blogs/edit-blog/edit-blog.component.ts
+++ src/app/features/blogs/edit-blog/edit-blog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NotificationService } from 'src/app/shared/services/notification.service';
+import { BlogsService } from '../blogs.service';
+import { Blogs } from '../models/blogs.model';
 
 @Component({
   selector: 'app-edit-blog',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-blog.component.scss']
 })
 export class EditBlogComponent implements OnInit {
+  title: string;
+  blog: Blogs;
 
-  constructor() { }
+  constructor(
+    private activedRoute: ActivatedRoute,
+    private blogService: BlogsService,
+    private notity: NotificationService
+  ) { }
 
   ngOnInit() {
+    this.title = this.activedRoute.snapshot.paramMap.get('id');
+    this.getBlog(this.title)
+  }
+
+  getBlog(title: string) {
+    this.blogService.getBlogByTitle(this.title).subscribe(blog => {
+      if (blog) {
+        this.blog = blog;
+      }
+    }, (err) => {
+      this.notity.showNotification('Something went wrong', 'top', 'error');
+    })
   }
 
 }
