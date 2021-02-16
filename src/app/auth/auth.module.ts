7f512ea5220388fcb8f2cd95df53c2268e50a249
niraj <niraj.kumar@plexcel.com>
2021-02-17 01:45:44 +0530

    Created new password and reset password screen

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -8,13 +8,15 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { AuthService } from './auth.service';
 import { SharedModule } from '../shared/shared.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { NewPasswordComponent } from './new-password/new-password.component';
 
 @NgModule({
   imports: [
     AuthPageRoutingModule,
     SharedModule
   ],
-  declarations: [AuthPage, SignInComponent, SignUpComponent],
+  declarations: [AuthPage, SignInComponent, SignUpComponent, ResetPasswordComponent, NewPasswordComponent],
   providers: [AuthService]
 })
 export class AuthPageModule {}
