a56552bb1e2b9952d3204a2dc1184a455ff8a7d0
niraj <niraj.kumar@plexcel.com>
2021-02-16 23:09:20 +0530

    Updated routing for blog componnet

--- src/app/features/blogs/blogs-routing.module.ts
+++ src/app/features/blogs/blogs-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { BlogsComponent } from './blogs.component';
+import { EditBlogComponent } from './edit-blog/edit-blog.component';
+import { ListOfBlogComponent } from './list-of-blog/list-of-blog.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: BlogsComponent,
+    children: [
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
+      { path: 'blogs', component: ListOfBlogComponent },
+      { path: 'create', component: AddBlogComponent },
+      { path: 'edit/:id', component: EditBlogComponent },
+      { path: ':id', component: BlogsComponent },
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/blogs/blogs.module.ts
+++ src/app/features/blogs/blogs.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { BlogsRoutingModule } from './blogs-routing.module';
 import { BlogsComponent } from './blogs.component';
+import { ListOfBlogComponent } from './list-of-blog/list-of-blog.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { EditBlogComponent } from './edit-blog/edit-blog.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
 
 
 @NgModule({
-  declarations: [BlogsComponent],
+  declarations: [BlogsComponent, ListOfBlogComponent, AddBlogComponent, EditBlogComponent, BlogDetailsComponent],
   imports: [
     CommonModule,
     BlogsRoutingModule
